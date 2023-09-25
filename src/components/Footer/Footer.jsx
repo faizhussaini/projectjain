7ef456288afb857bb50c8509fd65783215270461
Faiz_Hussaini <107918420+faizhussaini@users.noreply.github.com>
2023-09-25 15:52:32 +0530

    sdadw

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Whatsapp from '@iconscout/react-unicons/icons/uil-whatsapp';
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
 
