56db5087f145cca3e5d8e437aec9b79cf1ba6960
Faiz_Hussaini <107918420+faizhussaini@users.noreply.github.com>
2023-09-25 09:48:34 +0530

    drt1
    comi1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import "./Footer.css";
 import Wave from "../../img/wave.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Whatsapp from '@iconscout/react-unicons/icons/uil-whatsapp';
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
 
 const Footer = () => {
@@ -13,7 +14,7 @@ const Footer = () => {
         <span>syedfaiyazhussaini06@gmail.com</span>
         <div className="f-icons">
           <a href="https://instagram.com/ifaiz_hussaini?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"><Insta color="white" size={"3rem"} /></a>
-          <Facebook color="white" size={"3rem"} />
+          <a href="https://wa.link/4r68ui"><Whatsapp color="white" size={"3rem"} /></a>
           <a href="https://github.com/faizhussaini"><Gitub color="white" size={"3rem"} /></a>
         </div>
       </div>
